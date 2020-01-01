761c64f7e0d8d3b8c1c9fdbfde1992fa6eebc00e
Santiago Topham <santitopham@gmail.com>
2020-01-01 17:37:18 -0300

    Added child grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { GridComponent, FilterSettingsModel, ToolbarItems, EditSettingsModel, IEditCell, SaveEventArgs } from '@syncfusion/ej2-angular-grids';
-import { data } from './datasource';
+import { GridComponent, DetailRowService, GridModel, FilterSettingsModel, ToolbarItems, EditSettingsModel, IEditCell, SaveEventArgs } from '@syncfusion/ej2-angular-grids';
+import { data, employeeData } from './datasource';
 
 @Component({
     selector: 'app-root',
-    templateUrl: 'app.component.html'
+    templateUrl: 'app.component.html',
+    providers: [DetailRowService]
 })
 export class AppComponent implements OnInit {
 
-    public data: object[];
+    public pData: object[];
     @ViewChild('grid', {static: true})
     public grid: GridComponent;
     public filterOptions: FilterSettingsModel;
@@ -17,19 +18,38 @@ export class AppComponent implements OnInit {
     public orderData: object;
     public freightRules: object;
     public testOcultar = false;
+    public childGrid: GridModel = {
+      dataSource: data,
+      queryString: 'EmployeeID',
+      allowFiltering: true,
+      allowGrouping: true,
+      allowSorting: true,
+      allowPaging: true,
+      allowResizing: true,
+      allowReordering: true,
+      showColumnChooser: true,
+      filterSettings: { type: 'Excel'},
+      toolbar: ['Add', 'Edit', 'Delete', 'Update', 'Cancel', 'Search', 'ColumnChooser'],
+      editSettings: { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Normal', showDeleteConfirmDialog: true },
+      columns: [
+          { field: 'OrderID', headerText: 'Order ID', textAlign: 'Right', width: 120, isPrimaryKey: true },
+          { field: 'CustomerID', headerText: 'Customer ID', width: 150 },
+          { field: 'ShipCity', headerText: 'Ship City', width: 150, editType: 'dropdownedit' },
+          { field: 'ShipName', headerText: 'Ship Name', width: 150, editType: 'dropdownedit' }
+      ],
+  };
 
     dataBound() {
       Object.assign((this.grid.filterModule as any).filterOperators, { startsWith: 'contains' });
     }
 
     ngOnInit(): void {
-      this.data = data;
+      this.pData = employeeData;
       this.filterOptions = {
         type: 'Excel'
       };
       this.toolbarOptions = ['Add', 'Edit', 'Delete', 'Update', 'Cancel', 'Search', 'ColumnChooser'];
-      this.editSettings = { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Batch', showDeleteConfirmDialog: true }; //Normal - Dialog
-      // this.freightRules = { minValue: };
+      this.editSettings = { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Normal', showDeleteConfirmDialog: true }; //Normal - Dialog - Batch
     }
 
     actionBegin(args: SaveEventArgs) {
