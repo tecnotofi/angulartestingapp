9aa6a6b780c188542117b31e5b3448de858fccbd
Santiago Topham <santiago.topham@visitor.upm.com>
2020-01-02 14:51:34 -0300

    Started to change to use real data for testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { GridComponent, DetailRowService, GridModel, FilterSettingsModel, ToolbarItems, EditSettingsModel, IEditCell, SaveEventArgs } from '@syncfusion/ej2-angular-grids';
-import { data, employeeData } from './datasource';
+import { data, dataDos, employeeData } from './datasource';
+import { arauserData } from './datasourceBio';
 
 @Component({
     selector: 'app-root',
@@ -18,33 +19,33 @@ export class AppComponent implements OnInit {
     public orderData: object;
     public freightRules: object;
     public testOcultar = false;
-    public childGrid: GridModel = {
-      dataSource: data,
-      queryString: 'EmployeeID',
-      allowFiltering: true,
-      allowGrouping: true,
-      allowSorting: true,
-      allowPaging: true,
-      allowResizing: true,
-      allowReordering: true,
-      showColumnChooser: true,
-      filterSettings: { type: 'Excel'},
-      toolbar: ['Add', 'Edit', 'Delete', 'Update', 'Cancel', 'Search', 'ColumnChooser'],
-      editSettings: { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Normal', showDeleteConfirmDialog: true },
-      columns: [
-          { field: 'OrderID', headerText: 'Order ID', textAlign: 'Right', width: 120, isPrimaryKey: true },
-          { field: 'CustomerID', headerText: 'Customer ID', width: 150 },
-          { field: 'ShipCity', headerText: 'Ship City', width: 150, editType: 'dropdownedit' },
-          { field: 'ShipName', headerText: 'Ship Name', width: 150, editType: 'dropdownedit' }
-      ],
-  };
+    // public childGrid: GridModel = {
+    //   dataSource: data,
+    //   queryString: 'EmployeeID',
+    //   allowFiltering: true,
+    //   allowGrouping: true,
+    //   allowSorting: true,
+    //   allowPaging: true,
+    //   allowResizing: true,
+    //   allowReordering: true,
+    //   showColumnChooser: true,
+    //   filterSettings: { type: 'Excel'},
+    //   toolbar: ['Add', 'Edit', 'Delete', 'Update', 'Cancel', 'Search', 'ColumnChooser'],
+    //   editSettings: { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Normal', showDeleteConfirmDialog: true },
+    //   columns: [
+    //       { field: 'OrderID', headerText: 'Order ID', textAlign: 'Right', width: 120, isPrimaryKey: true },
+    //       { field: 'CustomerID', headerText: 'Customer ID', width: 150 },
+    //       { field: 'ShipCity', headerText: 'Ship City', width: 150, editType: 'dropdownedit' },
+    //       { field: 'ShipName', headerText: 'Ship Name', width: 150, editType: 'dropdownedit' }
+    //   ]
+  // };
 
     dataBound() {
       Object.assign((this.grid.filterModule as any).filterOperators, { startsWith: 'contains' });
     }
 
     ngOnInit(): void {
-      this.pData = employeeData;
+      this.pData = arauserData;
       this.filterOptions = {
         type: 'Excel'
       };
